e331f011af19a44ad313fcfe0198c5d53b89c994
mstaniucha <michal.staniucha@gmail.com>
2021-02-27 09:29:51 +0100

    Calculate sum of first 3 numbers #1

--- src/main/java/com/stringcalculator/StringCalculator.java
+++ src/main/java/com/stringcalculator/StringCalculator.java
@@ -1,8 +1,23 @@
 package com.stringcalculator;
 
+import java.util.stream.Stream;
+
 public class StringCalculator {
+    private static final String DELIMITER = ",";
 
     public int add(String input) {
-        return 0;
+        return isEmpty(input) ? 0 : summarize(input);
+
+    }
+
+    private boolean isEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    public int summarize(String input) {
+        return Stream.of(input.split(DELIMITER))
+                .limit(3)
+                .map((Integer::parseInt))
+                .reduce(0, Integer::sum);
     }
 }
--- src/test/java/com/stringcalculator/StringCalculatorTest.groovy
+++ src/test/java/com/stringcalculator/StringCalculatorTest.groovy
@@ -1,6 +1,7 @@
 package com.stringcalculator
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class StringCalculatorTest extends Specification {
     private def calculator = new StringCalculator()
@@ -10,11 +11,29 @@ class StringCalculatorTest extends Specification {
         calculator != null
     }
 
-    def "should return constant for each request "() {
+    @Unroll
+    def "should return sum of numbers separated by comma [input = #input]"() {
         expect:
-        calculator.add("s148104715fada") == 0
+        calculator.add(input) == output
 
         where:
-        input << ["s148104715fada", "5", "n"]
+        input     | output
+        ""        | 0
+        "1"       | 1
+        "1,2"     | 3
+        "1,2,3"   | 6
+        "1,2,3,4" | 6
+        "1,2,3,s" | 6
+    }
+
+    def "should throw error when invalid input is given"() {
+        when:
+        calculator.add(input)
+
+        then:
+        thrown(NumberFormatException)
+
+        where:
+        input << ["1,s", "x", "{", "\n"]
     }
 }
